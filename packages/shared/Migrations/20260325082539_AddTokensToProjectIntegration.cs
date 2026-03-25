8d38831bd01aa6158f8e2c78449d1f64796dde66
Tran Tan Phat <trantanphat2004lp@gmail.com>
2026-03-25 15:29:11 +0700

    fix nua ne qua met r

---
+++ packages/shared/Migrations/20260325082539_AddTokensToProjectIntegration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JiraGithubExport.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTokensToProjectIntegration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
